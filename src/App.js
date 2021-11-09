5c50e89378f765bbaef1d3ab9a273b516782ef1b
Carlos <murillocarlos99@hotmail.com>
2021-11-08 23:09:53 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import { app } from './fb';
 import './App.css';
 
-function App() {
+const App =()=> {
+
+  const [docus,setDocus]=useState([]);
+
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const labelfecha = e.target.fecha.value;
+    const labelcadera=e.target.cadera.value;
+    const labelcintura=e.target.cintura.value;
+    const labelumbilical=e.target.umbilical.value;
+    const labelbrazo=e.target.brazo.value;
+    const labeltoracica=e.target.toracica.value;
+    const labelpierna=e.target.pierna.value;
+    const labelpeso=e.target.peso.value;
+
+    if (!labelfecha) {
+      alert("coloca una fecha");
+      return;
+    }
+    const coleccionRef = app.firestore().collection("medidas");
+    const docu = await coleccionRef
+      .doc(labelfecha)
+      .set({ 
+        fecha:  labelfecha,
+        medidacadera:Number (labelcadera),
+        medidacintura:Number (labelcintura),
+        medidaumbilical:Number (labelumbilical),
+        medidabrazo:Number (labelbrazo),
+        medidatoracica:Number (labeltoracica),
+        medidapierna:Number (labelpierna),
+        peso:Number (labelpeso),
+      });
+    window.location = "/";
+
+  };
+
+  useEffect(async()=>{
+    const docuList=await app.firestore().collection("medidas").get();
+    setDocus(docuList.docs.map((doc)=>doc.data() ));
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App de trackeo de progreso</h1>
+      <div className="formcontainer">
+        <form onSubmit={submitHandler}>
+          <div className="form-group">
+            <label >¿Qué dia es?</label>
+            <input type="date" className="form-control" name="fecha"/>
+          </div>
+
+          <div className="form-group">
+            <label >Cadera</label>
+            <input type="number" className="form-control" placeholder="Cadera" name="cadera"/>
+          </div>
+
+          <div className="form-group">
+            <label >Cintura</label>
+            <input type="number" className="form-control" placeholder="Cintura" name="cintura"/>
+          </div>
+
+          <div className="form-group">
+            <label >Umbilical</label>
+            <input type="number" className="form-control" placeholder="Umbilical" name="umbilical"/>
+          </div>
+
+          <div className="form-group">
+            <label >Brazo</label>
+            <input type="number" className="form-control" placeholder="Brazo" name="brazo"/>
+          </div>
+
+          <div className="form-group">
+            <label >Toracica</label>
+            <input type="number" className="form-control" placeholder="Toracica" name="toracica"/>
+          </div>
+
+          <div className="form-group">
+            <label >Pierna</label>
+            <input type="number" className="form-control" placeholder="Pierna" name="pierna"/>
+          </div>
+
+          <div className="form-group">
+            <label >Peso</label>
+            <input type="number" className="form-control" placeholder="lbs" name="peso"/>
+          </div>
+
+ 
+          <button type="submit" className="btn btn-primary">Guardar</button>
+        </form>
+      </div>
+
+      <hr></hr>
+      <div>
+        <div>
+          {
+            docus.map((doc)=>{
+              return(
+                <div>
+                  <p>Fecha:{doc.fecha}</p>
+                  <p>Cadera:{doc.medidacadera}</p>
+                  <p>Cintura:{doc.medidacintura}</p>
+                  <p>Umbilical:{doc.medidaumbilical}</p>
+                  <p>Brazo:{doc.medidabrazo}</p>
+                  <p>Toracica:{doc.medidatoracica}</p>
+                  <p>Pierna:{doc.medidapierna}</p>
+                  <p>Peso:{doc.peso}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/fb.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+
+export const app=firebase.initializeApp ({
+    apiKey: "AIzaSyCVrnsCcdKMRsB7Yn77K71Ab4uepkpngmQ",
+    authDomain: "progressapp-32660.firebaseapp.com",
+    projectId: "progressapp-32660",
+    storageBucket: "progressapp-32660.appspot.com",
+    messagingSenderId: "381257541791",
+    appId: "1:381257541791:web:96b761043a4434c0fa1aaa",
+    measurementId: "G-E0PH8PSHPQ"
+});
+
